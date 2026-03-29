0528878a02a1f4db0cc8dffdb0199570ad9ef6de
Aaron Massey <03masseyaaron@gmail.com>
2026-03-28 20:01:51 -0500

    updated character movement

--- playerCharacter/character.gd
+++ playerCharacter/character.gd
@@ -1,72 +1,130 @@
 extends CharacterBody3D
+
 @export_category("Movement")
-@export var acceleration : float = 10.0
-@export var deceleration : float = 15.0
-@export var topSpeed : float = 100.0
-@export var jumpVelocity : float = 4.5
+@export var acceleration: float = 10.0
+@export var deceleration: float = 15.0
+@export var top_speed: float = 100.0
+@export var jump_velocity: float = 4.5
+@export var turn_friction: float = 3.0
+
+@export_category("Sprint & Stamina")
+@export var sprint_speed: float = 14.0
+@export var sprint_acceleration: float = 60.0
+@export var max_stamina: float = 100.0
+@export var stamina_drain_rate: float = 25.0
+@export var stamina_walk_recovery_rate: float = 10.0
+@export var stamina_idle_recovery_rate: float = 35.0
+
 @export_category("Camera Settings")
-@export var FOV : float = 75
-@export var TILT_LOWER_LIMIT : float = -90
-@export var TILT_UPPER_LIMIT : float = 90
-@export var MOUSE_SENSITIVITY : float = 0.5
+@export var fov: float = 75.0
+@export var tilt_lower_limit: float = -90.0
+@export var tilt_upper_limit: float = 90.0
+@export var mouse_sensitivity: float = 0.5
 
+var current_stamina: float = 100.0
+var is_sprinting: bool = false
+var is_exhausted: bool = false
+var _yaw_input: float
+var _pitch_input: float
 
-@onready var CAMERA_CONTROLLER = $Neck/Camera3D
+@onready var camera_controller: Camera3D = $Neck/Camera3D
 
-var _yaw_input : float #Camera left and right
-var _pitch_input : float #Camera up and down
 
 func _ready() -> void:
-	CAMERA_CONTROLLER.fov = FOV;
+	camera_controller.fov = fov
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	current_stamina = max_stamina
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		_yaw_input += -event.relative.x * MOUSE_SENSITIVITY
-		_pitch_input += -event.relative.y * MOUSE_SENSITIVITY
+		_yaw_input += -event.relative.x * mouse_sensitivity
+		_pitch_input += -event.relative.y * mouse_sensitivity
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	process_other_inputs()
 	move(delta)
-	updateCamera(delta)
+	update_camera()
 	move_and_slide()
-	
 
-func move(delta) -> void:
-	# Handle jump.
+
+func move(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jumpVelocity
+		velocity.y = jump_velocity
 
-	# Get the input direction
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	# Isolate our X/Z movement from our Y (gravity/jump) movement
-	var current_xz := Vector3(velocity.x, 0, velocity.z)
-	var target_xz : Vector3 = direction * topSpeed
-	
-	if direction:
-		# Accelerate the vector as a whole
-		current_xz = current_xz.move_toward(target_xz, acceleration * delta)
+
+	if current_stamina <= 0.0:
+		is_exhausted = true
+	elif current_stamina >= 20.0:
+		is_exhausted = false
+
+	if Input.is_action_pressed("move_sprint") and direction != Vector3.ZERO and not is_exhausted:
+		is_sprinting = true
 	else:
-		# Decelerate the vector as a whole
+		is_sprinting = false
+
+	if is_sprinting:
+		current_stamina -= stamina_drain_rate * delta
+	elif direction != Vector3.ZERO:
+		current_stamina += stamina_walk_recovery_rate * delta
+	else:
+		current_stamina += stamina_idle_recovery_rate * delta
+
+	current_stamina = clamp(current_stamina, 0.0, max_stamina)
+
+	var active_speed := sprint_speed if is_sprinting else top_speed
+	var active_accel := sprint_acceleration if is_sprinting else acceleration
+
+	var current_xz := Vector3(velocity.x, 0, velocity.z)
+	var target_xz: Vector3 = direction * active_speed
+
+	if direction:
+		var accel_multiplier := 1.0
+
+		if current_xz.length() > 0.5:
+			var momentum_alignment := direction.dot(current_xz.normalized())
+			if momentum_alignment < 0.9:
+				accel_multiplier = turn_friction
+
+		current_xz = current_xz.move_toward(target_xz, active_accel * accel_multiplier * delta)
+	else:
 		current_xz = current_xz.move_toward(Vector3.ZERO, deceleration * delta)
-		
-	# Apply the newly calculated X/Z values back to the actual velocity
+
 	velocity.x = current_xz.x
 	velocity.z = current_xz.z
 
-func updateCamera(delta) -> void:
+
+func update_camera() -> void:
 	rotate_y(deg_to_rad(_yaw_input))
-	
-	CAMERA_CONTROLLER.rotate_x(deg_to_rad(_pitch_input))
-	
-	var lowerLimit = deg_to_rad(TILT_LOWER_LIMIT)
-	var upperLimit = deg_to_rad(TILT_UPPER_LIMIT)
-	CAMERA_CONTROLLER.rotation.x = clamp(CAMERA_CONTROLLER.rotation.x, lowerLimit, upperLimit)
-	
+	camera_controller.rotate_x(deg_to_rad(_pitch_input))
+
+	var lower_limit := deg_to_rad(tilt_lower_limit)
+	var upper_limit := deg_to_rad(tilt_upper_limit)
+	camera_controller.rotation.x = clamp(camera_controller.rotation.x, lower_limit, upper_limit)
+
 	_yaw_input = 0.0
 	_pitch_input = 0.0
+
+
+func process_other_inputs() -> void:
+	if Input.is_action_just_pressed("debug_reset"):
+		set_pos(Vector3(0, 10, 0))
+	if Input.is_action_just_pressed("contain_mouse"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if Input.is_action_just_pressed("release_mouse"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func get_pos() -> Vector3:
+	return self.position
+
+
+func set_pos(new_pos: Vector3) -> void:
+	self.global_position = new_pos
+	self.velocity = Vector3.ZERO
