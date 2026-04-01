f9f8b50726c89a90baa332282fb2bb195e57137d
Nolen Jackson <nolenbolen@gmail.com>
2026-03-31 23:17:32 -0500

    Added pivot arm for lantern when colliding with objects to keep it visible

--- assets/playerCharacter/lantern.gd
+++ assets/playerCharacter/lantern.gd
@@ -2,10 +2,14 @@ extends Node3D
 
 @export var lantern_on = true
 @onready var oil_lantern = $"Sketchfab_Scene/Sketchfab_model/4b17dd9eba0245778cead7ee8fe19576_fbx/RootNode/Oil Lantern"
+@onready var target = $"../SpringArm3D/TargetPoint"
 
 func _ready() -> void:
 	$OmniLight3D.light_energy = 3.0 if lantern_on else 0.0
 	oil_lantern.set_lit(1.0 if lantern_on else 0.0)
+
+func _process(delta):
+	global_transform.origin = target.global_transform.origin + Vector3(0, -0.5, 0)
 
 func _input(event: InputEvent) -> void:
 	
