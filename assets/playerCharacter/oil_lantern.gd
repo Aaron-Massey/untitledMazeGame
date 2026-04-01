8c2a4cca3803951a5a50cd9f85daf79e5d94cad6
Nolen Jackson <nolenbolen@gmail.com>
2026-03-31 21:31:49 -0500

    Added ramp-up/ramp-down for lantern light. Also added fire particles

--- assets/playerCharacter/oil_lantern.gd
+++ assets/playerCharacter/oil_lantern.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
 @onready var candle_mesh = $"Oil Lantern_oil lantern light_0"
+@onready var fire = $GPUParticles3D
 
-func set_lit(is_lit: bool) -> void:
+func set_lit(value: float) -> void:
 	var mat = candle_mesh.get_active_material(0)
 	if mat:
-		mat.emission_enabled = is_lit
-	else:
-		print("Override material not set yet")
+		mat.emission_enabled = value > 0.0
+		mat.emission_energy_multiplier = value
+	fire.emitting = value > 0.0
+	if value == 0.0:
+		fire.restart()
+		fire.emitting = false
