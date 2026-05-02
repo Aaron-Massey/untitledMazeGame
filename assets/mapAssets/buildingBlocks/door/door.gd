d75e7467bcb6a6afb2b8fd67dc1f313712ea2c0f
Aaron Massey <03masseyaaron@gmail.com>
2026-05-02 17:12:13 -0500

    Created a basic maze

--- assets/mapAssets/buildingBlocks/door/door.gd
+++ assets/mapAssets/buildingBlocks/door/door.gd
@@ -1,10 +1,18 @@
 extends StaticBody3D
 
+# The speed the door opens at
 @export var door_speed: float = 1.0
+# Set this in the Inspector to match how many levers are connected to this door
+@export var required_levers: int = 2
+# Is the door currently open?
+@export var is_open: bool = false
 
 var closed_pos: Vector3
 var open_pos: Vector3
 var _tween: Tween
+
+# This will track how many levers are currently pulled
+var _active_levers_count: int = 0
 
 
 func _ready() -> void:
@@ -13,7 +21,17 @@ func _ready() -> void:
 
 
 func _on_lever_toggled(is_active: bool) -> void:
+	# Add or subtract from the total count based on the lever's state
 	if is_active:
+		_active_levers_count += 1
+	else:
+		_active_levers_count -= 1
+
+	# Safeguard to ensure the counter doesn't break if a lever bugs out
+	_active_levers_count = clampi(_active_levers_count, 0, required_levers)
+
+	# Only open if the active count matches the required amount
+	if _active_levers_count >= required_levers:
 		open_door()
 	else:
 		close_door()
@@ -22,11 +40,13 @@ func _on_lever_toggled(is_active: bool) -> void:
 func open_door() -> void:
 	_animate_door(open_pos)
 	print_debug("DOOR OPENED")
+	is_open = true
 
 
 func close_door() -> void:
 	_animate_door(closed_pos)
 	print_debug("DOOR CLOSED")
+	is_open = false
 
 
 func _animate_door(target_pos: Vector3) -> void:
